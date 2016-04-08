882b2afe07ab19144fd62114961929873aeb9fbc
g13943se <g13943se@s30409.ACTSUDA>
2016-04-08 11:28:00 +0900

    FirstHelloWorldです１

---
+++ src/firsthelloworld/FirstHelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firsthelloworld;
+
+/**
+ *
+ * @author g13943se
+ */
+public class FirstHelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
